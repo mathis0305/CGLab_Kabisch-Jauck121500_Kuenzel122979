3dccc22cd31a0341405b76711ab36bf89bd0a5b6
mathis0305 <82102820+mathis0305@users.noreply.github.com>
2023-04-26 15:49:58 +0200

    add node.cpp and node.hpp

---
+++ framework/include/node.hpp
@@ -0,0 +1,37 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <vector>
+#include <iostream>
+#include <glm/glm.hpp>
+
+class Node {
+ public:
+	 Node::Node();
+	 Node::Node(Node _parent, std::vector<Node> _children, std::string _name, std::string _path, int _depth, glm::mat4 _localTransformation, glm::mat4 _worldTransformation);
+	 Node getParent();
+	 void setParent(Node _parent);
+	 Node getChildren(std::string child);
+	 std::vector<Node> getChildrenList();
+	 std::string getName();
+	 std::string getPath();
+	 int getDepth();
+	 glm::mat4 getLocalTransformation();
+	 glm::mat4 getWorldTransformation();
+	 void setLocalTransformation(glm::mat4 _localTransformation);
+	 void setWorldTransformation(glm::mat4 _worldTransformation);
+	 void addChildren(Node newChild);
+	 std::string removeChildren(Node childToDelete);
+
+ 
+ private:
+	 static Node parent;
+	 static std::vector<Node> children;
+	 static std::string name;
+	 static std::string path;
+	 static int depth;
+	 static glm::mat4 localTransformation;
+	 static glm::mat4 worldTransformation;
+};
+
+#endif
---
+++ framework/source/node.cpp
@@ -0,0 +1,93 @@
+#include "node.hpp"
+
+Node::Node(Node _parent, std::vector<Node> _children, std::string _name, std::string _path, int _depth, glm::mat4 _localTransformation, glm::mat4 _worldTransformation) {
+	parent = _parent;
+	children = _children;
+	name = _name;
+	path = _path;
+	depth = _depth;
+	localTransformation = _localTransformation;
+	worldTransformation = _worldTransformation;
+}
+
+Node::Node() {
+	name = "";
+}
+
+Node Node::getParent()
+{
+	return parent;
+}
+
+void Node::setParent(Node _parent)
+{
+	parent = _parent;
+}
+
+Node Node::getChildren(std::string child)
+{
+	for (Node currentChild : children)
+		if (currentChild.name == child)
+			return currentChild;
+	std::cout << "Child not found" << std::endl;
+}
+
+std::vector<Node> Node::getChildrenList()
+{
+	return children;
+}
+
+std::string Node::getName()
+{
+	return name;
+}
+
+std::string Node::getPath()
+{
+	return path;
+}
+
+int Node::getDepth()
+{
+	return depth;
+}
+
+glm::mat4 Node::getLocalTransformation()
+{
+	return localTransformation;
+}
+
+glm::mat4 Node::getWorldTransformation()
+{
+	return worldTransformation;
+}
+
+void Node::setLocalTransformation(glm::mat4 _localTransformation)
+{
+	localTransformation = _localTransformation;
+}
+
+void Node::setWorldTransformation(glm::mat4 _worldTransformation)
+{
+	worldTransformation = _worldTransformation;
+}
+
+void Node::addChildren(Node newChild)
+{
+	children.push_back(newChild);
+}
+
+std::string Node::removeChildren(Node childToDelete)
+{
+	std::string currentName = "";
+	for (int i = 0; i < children.size(); i++)
+	{
+		currentName = children[i].name;
+		if (currentName == childToDelete.name)
+		{
+			children.erase(children.begin() + i);
+			return currentName;
+		}
+	}
+	return "";
+}
